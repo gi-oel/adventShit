4a7ab16d34292a0da8d14267d539355c222da3af
Gioele Petrillo <gioele.petrillo@neolution.ch>
2022-12-01 13:46:32 +0100

    reformat package

--- src/AdventOfCode2021/Day2.java
+++ src/AdventOfCode2021/Day2.java
@@ -1,3 +1,5 @@
+package AdventOfCode2021;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
--- src/AdventOfCode2021/Day3.java
+++ src/AdventOfCode2021/Day3.java
@@ -1,14 +1,14 @@
+package AdventOfCode2021;
+
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Day3 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         try {
             BufferedReader r = new BufferedReader(new FileReader(Paths.get("submarineEnergy.txt").toFile()));
             String number = r.readLine();
