3364e6d299afdbd66e8762c67dc764d8d8fef3c3
Gioele Petrillo <gioele.petrillo@neolution.ch>
2022-12-06 13:53:48 +0100

    start

---
+++ src/AdventOfCode2022/Day3.java
@@ -0,0 +1,12 @@
+package AdventOfCode2022;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Day3 {
+    public static void main(String[] args) throws IOException {
+        String ganzerInput = Files.readString(Paths.get("AdventOfCode2022Input/rucksack.txt"), StandardCharsets.UTF_8);
+    }
+}
