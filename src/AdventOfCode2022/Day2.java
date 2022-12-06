e71e6301ec2f77ad3883fb5449e21e423ff98dd8
Gioele Petrillo <gioele.petrillo@neolution.ch>
2022-12-06 08:39:54 +0100

    scores

---
+++ src/AdventOfCode2022/Day2.java
@@ -0,0 +1,14 @@
+package AdventOfCode2022;
+
+public class Day2 {
+    public static void main(String[] args) {
+        var points = new Object() {
+            int rock = 1;
+            int paper = 2;
+            int scissor = 3;
+            int lose = 0;
+            int draw = 3;
+            int win = 6;
+        };
+    }
+}
