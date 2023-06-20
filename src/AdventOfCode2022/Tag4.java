952d1770cfe6aab40b478aa11d3cafa5931b23af
Gioele Petrillo <53406238+tschogge@users.noreply.github.com>
2023-06-20 17:11:32 +0200

    tag 4 fertig

--- src/AdventOfCode2022/Tag4.java
+++ src/AdventOfCode2022/Tag4.java
@@ -14,6 +14,7 @@ public class Tag4 {
 
         // durch alle sektionen durchgehen
         int anzahlUeberschreitungen = 0;
+        int anzahlAlleUeberschreitungen = 0;
         for (String paar : geteiltePaare) {
             // Aufteilen in Sektionen
             String[] sektionen = paar.split(",");
@@ -22,13 +23,22 @@ public class Tag4 {
             int[] ersteSektion = Arrays.stream(sektionen[0].split("-")).mapToInt(Integer::parseInt).toArray();
             int[] zweiteSektion = Arrays.stream(sektionen[1].split("-")).mapToInt(Integer::parseInt).toArray();
 
-            // Überprüfen, ob das erste paar das andere direkt überschreitet
+            // Überprüfen, ob das erste paar das andere direkt überschreitet oder das zweite das erste
             if (ersteSektion[0] >= zweiteSektion[0] && ersteSektion[1] <= zweiteSektion[1] || zweiteSektion[0] >= ersteSektion[0] && zweiteSektion[1] <= ersteSektion[1]) {
                 anzahlUeberschreitungen++;
+            }
+
+            // Überprüfen, ob sich irgendwas überschreitet
+            if (ersteSektion[0] >= zweiteSektion[0] && ersteSektion[0] <= zweiteSektion[1]
+                    || ersteSektion[1] >= zweiteSektion[0] && ersteSektion[1] <= zweiteSektion[1]
+                    || zweiteSektion[0] >= ersteSektion[0] && zweiteSektion[0] <= ersteSektion[0]
+                    || zweiteSektion[1] >= ersteSektion[0] && zweiteSektion[1] <= ersteSektion[1]) {
+                anzahlAlleUeberschreitungen++;
             }
         }
 
         System.out.println("Anzahl Überschreitungen: " + anzahlUeberschreitungen);
+        System.out.println("Anzahl Alle Überschreitungen: " + anzahlAlleUeberschreitungen);
 
     }
 }
