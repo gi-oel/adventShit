0c23d7ac5014bc18e03aa940f5dda847288022db
Gioele Petrillo <53406238+tschogge@users.noreply.github.com>
2023-06-20 17:22:32 +0200

    weiter

--- src/AdventOfCode2022/Tag5.java
+++ src/AdventOfCode2022/Tag5.java
@@ -1,7 +1,15 @@
 package AdventOfCode2022;
 
-public class Tag5 {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class Tag5 {
+    public static void main(String[] args) throws IOException {
+        // Alles auslesen und in ein array schreiben
+        String alles = Files.readString(Paths.get("AdventOfCode2022Input/craneBoxes.txt"), StandardCharsets.UTF_8);
+
+        System.out.println(alles);
     }
 }
