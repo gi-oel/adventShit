c155ec29829b11879b903672d8964490c2c359df
Gioele Petrillo <53406238+tschogge@users.noreply.github.com>
2023-06-20 16:42:20 +0200

    erster teil fertig

--- src/AdventOfCode2022/Tag4.java
+++ src/AdventOfCode2022/Tag4.java
@@ -8,8 +8,27 @@ import java.util.Arrays;
 
 public class Tag4 {
     public static void main(String[] args) throws IOException {
+        // Alles auslesen und in ein array schreiben
         String alleSektionen = Files.readString(Paths.get("AdventOfCode2022Input/sections.txt"), StandardCharsets.UTF_8);
-        String[] geteilteSektionen = alleSektionen.split("[,]|[(\\r\\n)]");
-        System.out.println(Arrays.toString(geteilteSektionen));
+        String[] geteiltePaare = alleSektionen.split("\n");
+
+        // durch alle sektionen durchgehen
+        int anzahlUeberschreitungen = 0;
+        for (String paar : geteiltePaare) {
+            // Aufteilen in Sektionen
+            String[] sektionen = paar.split(",");
+
+            // Aufteilen in einzelne Zahlen
+            int[] ersteSektion = Arrays.stream(sektionen[0].split("-")).mapToInt(Integer::parseInt).toArray();
+            int[] zweiteSektion = Arrays.stream(sektionen[1].split("-")).mapToInt(Integer::parseInt).toArray();
+
+            // Überprüfen, ob das erste paar das andere direkt überschreitet
+            if (ersteSektion[0] >= zweiteSektion[0] && ersteSektion[1] <= zweiteSektion[1] || zweiteSektion[0] >= ersteSektion[0] && zweiteSektion[1] <= ersteSektion[1]) {
+                anzahlUeberschreitungen++;
+            }
+        }
+
+        System.out.println("Anzahl Überschreitungen: " + anzahlUeberschreitungen);
+
     }
 }
