08999e147a1c82ea6163558efe45e9e447bfee33
Gioele Petrillo <53406238+tschogge@users.noreply.github.com>
2023-06-21 09:56:24 +0200

    umgestapelt

--- src/AdventOfCode2022/Tag5.java
+++ src/AdventOfCode2022/Tag5.java
@@ -4,12 +4,31 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.*;
 
 public class Tag5 {
     public static void main(String[] args) throws IOException {
         // Alles auslesen und in ein array schreiben
-        String alles = Files.readString(Paths.get("AdventOfCode2022Input/craneBoxes.txt"), StandardCharsets.UTF_8);
+        List<String> alleSchritte = Files.readAllLines(Paths.get("AdventOfCode2022Input/craneBoxes.txt"), StandardCharsets.UTF_8);
 
-        System.out.println(alles);
+        // Liste mit den Boxen machen
+        // Listen in ein array machen, damit es einfacher ist
+        var alleStapel = new List[]{
+                List.of("S", "P", "W", "N", "J", "Z"),
+                List.of("T", "S", "G"),
+                List.of("H", "L", "R", "Q", "V"),
+                List.of("D", "T", "S", "V"),
+                List.of("J", "M", "B", "D", "T", "Z", "Q"),
+                List.of("L", "Z", "C", "D", "J", "T", "W", "M"),
+                List.of("J", "T", "G", "W", "M", "P", "L"),
+                List.of("H", "Q", "F", "B", "T", "M", "G", "N"),
+                List.of("W", "Q", "B", "P", "P", "C", "G", "D", "R")};
+
+
+        // Durch alle Schritte durchgehen
+        for (String schritt : alleSchritte) {
+            // Schritt aufteilen um zu verstehen was zu tun ist. 1, 3, 5 wird gebraucht
+            var anleitung = schritt.split(" ");
+        }
     }
 }
